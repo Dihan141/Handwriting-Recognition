c87c8c1c51f71b154be39205b68fd17b90566bd3
GoonerMAK <mashrurahsan@iut-dhaka.edu>
2023-12-28 22:53:49 +0600

    Interactive version of the Model
    - Has multiple instances of models
    - Has a Fully working model with 150 iterations
    - Has a canvas for real time predictions

--- inferenceModel.py
+++ inferenceModel.py
@@ -5,6 +5,8 @@ import numpy as np
 from mltu.inferenceModel import OnnxInferenceModel
 from mltu.utils.text_utils import ctc_decoder, get_cer
 
+import os
+from mltu.tensorflow.dataProvider import DataProvider
 
 class ImageToWordModel(OnnxInferenceModel):
     def __init__(self, char_list: typing.Union[str, list], *args, **kwargs):
@@ -13,6 +15,8 @@ class ImageToWordModel(OnnxInferenceModel):
 
     def predict(self, image: np.ndarray):
         image = cv2.resize(image, self.input_shape[:2][::-1])
+        
+        # image = image.astype(np.float32) / 255.0
 
         image_pred = np.expand_dims(image, axis=0).astype(np.float32)
 
@@ -27,15 +31,57 @@ if __name__ == "__main__":
     import pandas as pd
     from tqdm import tqdm
     from mltu.configs import BaseModelConfigs
+    from mltu.preprocessors import ImageReader
+    from mltu.transformers import ImageResizer, LabelIndexer, LabelPadding, ImageShowCV2
+    from mltu.annotations.images import CVImage
 
-    configs = BaseModelConfigs.load("Models/03_handwriting_recognition/202301111911/configs.yaml")
-
+    configs = BaseModelConfigs.load("Models/03_handwriting_recognition/202311290851/configs.yaml")
+    
     model = ImageToWordModel(model_path=configs.model_path, char_list=configs.vocab)
 
-    df = pd.read_csv("Models/03_handwriting_recognition/202301111911/val.csv").values.tolist()
+    df = pd.read_csv("Models/03_handwriting_recognition/202311290851/val.csv").values.tolist()
 
     accum_cer = []
-    for image_path, label in tqdm(df):
+    
+    dataset_path = "IAM_Tests"
+    dataset, vocab, max_len = [], set(), 0
+    words = open("IAM_Tests\words.txt", "r").readlines()
+    for line in tqdm(words):
+        if line.startswith("#"):
+            continue
+
+        line_split = line.split(" ")
+        if line_split[1] == "err":
+            continue
+
+        folder1 = line_split[0][:3]
+        folder2 = "-".join(line_split[0].split("-")[:2])
+        file_name = line_split[0] + ".png"
+        label = line_split[-1].rstrip("\n")
+
+        rel_path = os.path.join(dataset_path, folder1, folder2, file_name)
+        if not os.path.exists(rel_path):
+            print(f"File not found: {rel_path}")
+            continue
+
+        dataset.append([rel_path, label])
+        vocab.update(list(label))
+        max_len = max(max_len, len(label))
+        
+        data_provider = DataProvider(
+        dataset=dataset,
+        skip_validation=True,
+        batch_size=configs.batch_size,
+        data_preprocessors=[ImageReader(CVImage)],
+        transformers=[
+        ImageResizer(configs.width, configs.height, keep_aspect_ratio=False),
+        LabelIndexer(configs.vocab),
+        LabelPadding(max_word_length=configs.max_text_length, padding_value=len(configs.vocab)),
+        ],
+    )
+    
+    for image_path, label in tqdm(dataset):
+        
         image = cv2.imread(image_path)
 
         prediction_text = model.predict(image)
@@ -52,3 +98,4 @@ if __name__ == "__main__":
         cv2.destroyAllWindows()
 
     print(f"Average CER: {np.average(accum_cer)}")
+    print(f"{configs.model_path}")
